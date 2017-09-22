3978226d4c0ce3c15b31d43a220c20912756a3fd
dragoaa90@gmail.com <dragoa90@gmail.com>
2017-09-22 14:30:38 +0900

    문자열 변경 추가

--- Hughtill/Hughtill/Htills/NSString+Htills.h
+++ Hughtill/Hughtill/Htills/NSString+Htills.h
@@ -7,6 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 @interface NSString(Htills)
 
@@ -15,5 +16,18 @@
 
 //마지막 글자수(count)만큼 문자 제거
 - (NSString*) removeCountStringWithlengthCount:(int)count;
+
+//문자열안의 특정 텍스트(before)를 변경(afterString)
+- (NSString*) setChangeString:(NSString*)beforeString afterString:(NSString*)afterString;
+
+//문자열안의 특정 텍스트(standard)를 기준으로 NSArray에 담기
+- (NSArray*) getDivisionStringWithStandardString:(NSString*)standardString;
+
+//문자열안의 특정 텍스트 색상 변경
+- (NSAttributedString*) setCertainStringChangeColorWithString:(NSString*)string color:(UIColor*)color;
+
+//문자열안의 특정 텍스트 폰트 변경
+- (NSAttributedString*) setCertainStringChangeFontWithString:(NSString*)string font:(UIFont*)font;
+
 
 @end
--- Hughtill/Hughtill/Htills/NSString+Htills.m
+++ Hughtill/Hughtill/Htills/NSString+Htills.m
@@ -15,11 +15,62 @@
 {
     return [self removeCountStringWithlengthCount:1];
 }
+
 //마지막 글자수(count)만큼 문자 제거
 - (NSString*) removeCountStringWithlengthCount:(int)count
 {
     NSString *newString = [self substringToIndex:[self length]-count];
     return newString;
+}
+
+//문자열안의 특정 텍스트(before)를 변경(afterString)
+- (NSString*) setChangeString:(NSString*)beforeString afterString:(NSString*)afterString
+{
+    return [self stringByReplacingOccurrencesOfString:beforeString withString:afterString];
+}
+
+//문자열안의 특정 텍스트(standard)를 기준으로 NSArray에 담기
+- (NSArray*) getDivisionStringWithStandardString:(NSString*)standardString
+{
+    return [self componentsSeparatedByString:standardString];
+}
+
+//문자열안의 특정 텍스트 색상 변경
+- (NSAttributedString*) setCertainStringChangeColorWithString:(NSString*)string color:(UIColor*)color
+{
+    //문자열 설정
+    NSMutableAttributedString* att = [[NSMutableAttributedString alloc] initWithString:self];
+    
+    //변화시킬 범위를 설정
+    NSRange range = [self rangeOfString:string];
+    
+    //변화종류, 옵션 설정
+    [att addAttribute:NSForegroundColorAttributeName
+                value:[color colorWithAlphaComponent:1.0f]//[UIColor brownColor]만 해도 사용이 가능합니다
+                range:range];
+    return att;
+    //설정내용으로 라벨 변경
+    //[label setAttributedText:att];//적용법
+}
+
+//문자열안의 특정 텍스트 폰트 변경
+- (NSAttributedString*) setCertainStringChangeFontWithString:(NSString*)string font:(UIFont*)font
+{
+    
+    //문자열 설정
+    NSMutableAttributedString* att = [[NSMutableAttributedString alloc] initWithString:self];
+    
+    //변화시킬 범위를 설정
+    NSRange range = [self rangeOfString:string];
+    
+    //변화종류, 옵션 설정
+    [att addAttribute:NSFontAttributeName
+                value:font
+                range:range];
+    
+    return att;
+    //설정내용으로 라벨 변경
+    //[label setAttributedText:att];//적용법
 }
 
 @end
