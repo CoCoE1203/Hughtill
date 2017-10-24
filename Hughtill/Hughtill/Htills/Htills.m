2aeceab543d95b81011725a64a347dfb6e92aaf6
dragoaa90@gmail.com <dragoa90@gmail.com>
2017-10-24 14:53:54 +0900

    refresh

--- Hughtill/Hughtill/Htills/Htills.m
+++ Hughtill/Hughtill/Htills/Htills.m
@@ -89,18 +89,22 @@
 {
     if ([responseObject isKindOfClass:[NSArray class]] || [responseObject isKindOfClass:[NSMutableArray class]])
     {
-        NSMutableArray* decodeArray = [NSMutableArray new];
-        for (id value in responseObject)
-        {
-            [decodeArray addObject:[Htills checkWithNullData:value]];
-        }
-        return decodeArray;
+        return [Htills checkArray:responseObject];
     }
     
     else if ([responseObject isKindOfClass:[NSMutableDictionary class]] || [responseObject isKindOfClass:[NSDictionary class]])
     {
+        return [Htills checkDictionary:responseObject];
+    }
+    else return [Htills checkWithNullData:responseObject];
+}
+
++ (id) checkDictionary:(id)dictionary
+{
+    if ([dictionary isKindOfClass:[NSMutableDictionary class]] || [dictionary isKindOfClass:[NSDictionary class]])
+    {
         NSMutableDictionary* decodeDictionary = [NSMutableDictionary new];
-        NSDictionary* object = responseObject;
+        NSDictionary* object = dictionary;
         NSArray* objectValue = object.allValues;
         NSArray* objectKeys  = object.allKeys;
         
@@ -109,17 +113,48 @@
         {
             if ([value isKindOfClass:[NSMutableDictionary class]] || [value isKindOfClass:[NSDictionary class]])
             {
-                
+                //dictionary안에 dictionary발견
+                [decodeDictionary setObject:[Htills checkDictionary:value] forKey:objectKeys[count]];
             }
-            [decodeDictionary setObject:[Htills checkWithNullData:value] forKey:objectKeys[count]];
+            else if ([value isKindOfClass:[NSMutableArray class]] || [value isKindOfClass:[NSArray class]])
+            {
+                //dictionary안에 array발견
+                [decodeDictionary setObject:[Htills checkArray:value] forKey:objectKeys[count]];
+            }
+            else
+                [decodeDictionary setObject:[Htills checkWithNullData:value] forKey:objectKeys[count]];
             count++;
         }
         
-        
         return decodeDictionary;
+        
     }
-    
-    else return nil;
+    else return dictionary;
+}
+
++ (id) checkArray:(id)array
+{
+    if ([array isKindOfClass:[NSMutableArray class]] || [array isKindOfClass:[NSArray class]])
+    {
+        NSMutableArray* decodeArray = [NSMutableArray new];
+        for (id value in array)
+        {
+            if ([value isKindOfClass:[NSMutableDictionary class]] || [value isKindOfClass:[NSDictionary class]])
+            {
+                //array안에 dictionary발견
+                [decodeArray addObject:[Htills checkDictionary:value]];
+            }
+            else if ([value isKindOfClass:[NSMutableArray class]] || [value isKindOfClass:[NSArray class]])
+            {
+                //array안에 array발견
+                [decodeArray addObject:[Htills checkArray:value]];
+            }
+            else//null check
+                [decodeArray addObject:[Htills checkWithNullData:value]];
+        }
+        return decodeArray;
+    }
+    else return array;
 }
 
 + (id) checkWithNullData:(id)value
@@ -134,16 +169,18 @@
 
 + (BOOL) checkStringValue:(id)value
 {
-    if ([value isKindOfClass:[NSString class]])
+    if ([value isKindOfClass:[NSString class]] || [value isKindOfClass:[NSMutableString class]])
     {
         NSString* stringValue = value;
         if (stringValue.length <= 0) return NO;
         
         if (    [value isEqualToString:@"null"]     ||
-                [value isEqualToString:@"NULL"]     ||
-                [value isEqualToString:@"<null>"]   ||
-                [value isEqualToString:@"<NULL>"]   )   return YES;
+            [value isEqualToString:@"NULL"]     ||
+            [value isEqualToString:@"<null>"]   ||
+            [value isEqualToString:@"<NULL>"]   )   return YES;
     }
+    else if ([value isKindOfClass:[NSNull class]]) return YES;
+    
     return NO;
 }
 
