79866a8ecb6d952dce2f812ee5514e96ce61f962
Hugh_Park <park_kyeongjoon@chinada.co.kr>
2018-01-05 17:21:39 +0900

    test

--- Hughtill/Hughtill/ViewController.m
+++ Hughtill/Hughtill/ViewController.m
@@ -12,13 +12,131 @@
 #import "Htills.h"
 
 @interface ViewController ()
-
+{
+    NSInteger selectIndex;
+}
 @end
 
 @implementation ViewController
 
+- (void) viewDidLoad
+{
+    [super viewDidLoad];
+    self.tableData = [NSMutableArray new];
+    selectIndex = -1;
+    self.TableView.rowHeight = UITableViewAutomaticDimension;
+    self.TableView.estimatedRowHeight = 80;
+
+    [self.tableData addObject:@{
+                                @"detail":
+                                @"공지사항 내용"
+                                }];
+    [self.tableData addObject:@{
+                                @"detail":
+                                @"공지사항 내용공지사항 내용공지사항 내용공지사항 내용공지사항 내용공지사항 내용공지사항 내용공지사항 내용공지사항 내용공지사항 내용공지사항 내용공지사항 내용공지사항 내용공지사항 내용공지사항 내용공지사항 내용공지사항 내용공지사항 내용공지사항 내용공지사항 내용공지사항 내용공지사항 내용공지사항 내용공지사항 내용"
+                                }];
+    [self.TableView reloadData];
+}
+
+- (NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.tableData.count;
+}
+
+- (UITableViewCell*) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSDictionary* data = self.tableData[indexPath.row];
+    
+    if (indexPath.row == selectIndex)//선택한 공지사항
+    {
+        UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier:@"openCell"];
+        [cell setSelectionStyle:UITableViewCellSelectionStyleGray];
+        UILabel* textLabel = [cell.contentView viewWithTag:100];
+        [textLabel setText:[data objectForKey:@"detail"]];
+        return cell;
+    }
+    else
+    {
+        UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier:@"closeCell"];
+        [cell setSelectionStyle:UITableViewCellSelectionStyleGray];
+        return cell;
+    }
+    
+    return [UITableViewCell new];
+}
+
+
+
+- (void) tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    //같은걸 다시 눌렀다면.
+    if (selectIndex == indexPath.row) {
+        selectIndex = -1;
+        [tableView reloadRowsAtIndexPaths:@[ indexPath ] withRowAnimation:UITableViewRowAnimationAutomatic];
+    }
+    else
+    {
+        NSInteger prevIndex = selectIndex;
+        selectIndex = indexPath.row;
+        NSMutableArray* reloadIndexes = [NSMutableArray new];
+        
+        if (selectIndex != -1)
+        {
+            NSIndexPath* prevIndexPath = [NSIndexPath indexPathForRow:prevIndex inSection:0];
+            [reloadIndexes addObject:prevIndexPath];
+            //[tableView reloadRowsAtIndexPaths:@[ prevIndexPath ] withRowAnimation:UITableViewRowAnimationFade];
+        }
+        
+        [reloadIndexes addObject:indexPath];
+        [tableView reloadRowsAtIndexPaths:reloadIndexes withRowAnimation:UITableViewRowAnimationAutomatic];
+    }
+
+    
+    
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
 }
 
 - (void) viewWillAppear:(BOOL)animated
@@ -42,7 +160,7 @@
 {
     
 }
-
+*/
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
